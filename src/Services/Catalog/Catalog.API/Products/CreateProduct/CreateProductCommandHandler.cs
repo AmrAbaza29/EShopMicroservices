4934c8e520a734b49e06f5726b74db1dad6888a0
AmrAbaza29 <134836859+AmrAbaza29@users.noreply.github.com>
2024-06-22 20:40:02 +0300

    create-product

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand>
+    {
+        public async Task<Unit> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = request.Name,
+                Category = request.Category,
+                Description = request.Description,
+                ImageFile = request.ImageFile,
+                Price = request.Price
+            };
+
+            //save to db 
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductRequest(string Name, List<string>Category, string Description, string ImageFile, decimal Price);
+    public class CreateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async(CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+                await sender.Send(command);
+                return Results.Created();
+            }).WithName("CreateProduct")
+            .Produces(201)
+            .ProducesProblem(400)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
+
+        }
+    }
+}
