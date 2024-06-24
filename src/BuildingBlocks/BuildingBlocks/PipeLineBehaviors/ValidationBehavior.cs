2f9b9316c16fddc1bd72e201d5d29e53d4b8af23
AmrAbaza29 <134836859+AmrAbaza29@users.noreply.github.com>
2024-06-24 21:57:18 +0300

    add-FluentValidation

---
+++ src/BuildingBlocks/BuildingBlocks/PipeLineBehaviors/ValidationBehavior.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using MediatR;
+
+namespace BuildingBlocks.PipeLineBehaviors
+{
+    public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) 
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : ICommand
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var validationResult = await Task.WhenAll
+                (validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+            var validationErrors = validationResult
+                .Where(v=>v.Errors.Any())
+                .SelectMany(v=>v.Errors)
+                .ToList();
+
+            if(validationErrors.Any())
+                throw new ValidationException(validationErrors);
+
+            return await next();
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,12 +1,54 @@
-
+using BuildingBlocks.Exceptions.Handler;
+using BuildingBlocks.PipeLineBehaviors;
 using Carter;
+using FluentValidation;
+using Marten;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCarter();
-builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 
+builder.Services.AddCarter();
+
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("CatalogDB")!);
+}).UseLightweightSessions();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 var app = builder.Build();
 app.MapCarter();
+app.UseExceptionHandler(options => { });
+//app.UseExceptionHandler(exceptionHandlerApp =>
+//{
+//    exceptionHandlerApp.Run(async context =>
+//    {
+//        var ex = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+//        if (ex is null)
+//            return;
+
+//        var problemDetails = new ProblemDetails
+//        {
+//            Title = ex.Message,
+//            Detail = ex.StackTrace,
+//            Status = StatusCodes.Status500InternalServerError
+//        };
+
+//        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+//        logger.LogError(ex, ex.Message);
+
+//        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+//        context.Response.ContentType = "application/problem+json";
+
+//        await context.Response.WriteAsJsonAsync(problemDetails);
+//    });
+//});
 
 app.Run();
