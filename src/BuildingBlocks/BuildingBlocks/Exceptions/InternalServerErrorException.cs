684555cb782ba5332cfbfd3d00c85f2f70b40562
AmrAbaza29 <134836859+AmrAbaza29@users.noreply.github.com>
2024-06-24 21:55:47 +0300

    Add-Global-ExceptionHandler

---
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/InternalServerErrorException.cs
@@ -0,0 +1,16 @@
+﻿namespace BuildingBlocks.Exceptions
+{
+    public class InternalServerErrorException : Exception
+    {
+        public InternalServerErrorException()
+        {
+            
+        }
+
+        public InternalServerErrorException(string message, string details):base(message)
+        {
+            Details = details;
+        }
+        public string Details { get; }
+    }
+}
